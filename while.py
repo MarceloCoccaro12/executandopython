97cae9afce5755ae5e7a2344787e54b5c2473d07
Marcelo <marcelococcaro@email.com>
2024-03-06 21:26:14 -0300

    Testando segundo comando while

--- while.py
+++ while.py
@@ -1,4 +1,16 @@
-x= 1;
-while x<=15:
-    print(x)
-    x=x+1
+qtd=0
+soma=0
+media=0
+valor= float(input("Digite um valor:"))
+
+while (valor > 0.0):
+    soma= soma + valor
+    qtd= qtd + 1
+    #Entrada de valores
+    valor = float(input("Digite um valor:"))
+
+#caso  digite um valor negativo o laco encerra
+meida = soma / qtd
+print("\n Total da Soma:", soma)
+print("\n Quantidade de valores digitados:", qtd)
+print("\n Média dos valores:", media)
